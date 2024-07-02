77a7b535525ed43c897b462933223971be8c6de5
Tony Xiao <tonyx.ca@gmail.com>
2024-07-02 00:10:02 -0700

    Reproduction for the unhandledRejection: ConnectJS wont load error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
+'use client'
+import { loadConnectAndInitialize } from '@stripe/connect-js/pure'
+import Image from 'next/image'
+import React from 'react'
 
 export default function Home() {
+  const [instance] = React.useState(() =>
+    loadConnectAndInitialize({
+      fetchClientSecret: async () => '',
+      publishableKey: '',
+    }),
+  )
+  console.log(instance)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -15,7 +26,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -47,7 +58,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +75,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            Learn{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +92,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Templates{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +109,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Deploy{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -109,5 +120,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
+  )
 }
